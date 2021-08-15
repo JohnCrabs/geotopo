ba18ad65b8e0dbe8492d4cad85eb908f0649476b
JohnCrabs <kavouras.ioannis@gmail.com>
2021-08-15 22:03:32 +0300

    Create "file.py" file

---
+++ Lib/file.py
@@ -0,0 +1,27 @@
+"""
+My python library for file read/write usage.
+"""
+import os
+import warnings
+
+
+def checkFile(path):
+    if os.path.isfile(path):  # If path is file
+        return True  # return True
+    else:  # else
+        msg = "File does not exist! Maybe wrong path?"  # create warning message
+        warnings.warn(msg)  # Show warning in console (this will point to log file in future update)
+        return False  # return False
+
+
+def read_TXT(path: str):
+    """
+    This function checks if the file exists and if exists return the file to a string.
+    :param path: path to the file
+    :return: a string with the contain of the file
+    """
+    data = ''  # Create an empty data string
+    if checkFile(path):  # Check if file exists
+        with open(path, "r") as myFile:  # Open the file if exists
+            data = myFile.readlines()  # Read the file
+    return data  # return data string
